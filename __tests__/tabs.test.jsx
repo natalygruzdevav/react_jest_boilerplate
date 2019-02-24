bfa0a94fdeb36a54e9dfd130b7f50f4504972a48
Nataly Gruzdeva <gruzdeeva@restream.rt.ru>
2019-02-24 18:20:22 +0400

    rss paring

--- __tests__/tabs.test.jsx
+++ __tests__/tabs.test.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { mount } from "enzyme";
-import "react-log-state";
-import nock from "nock";
-import delay from "delay";
-import path from "path";
+import React from 'react';
+import { mount } from 'enzyme';
+import 'react-log-state';
+import nock from 'nock';
+import delay from 'delay';
 
-import App from "../src/components/App";
+import App from '../src/components/App';
 
 // eslint-disable-next-line
 ReactLogState.logAll();
@@ -22,47 +21,46 @@ const buildSelectors = wrapper => ({
   tabBox: () => wrapper.find(tabBox),
   btnRemove: () => wrapper.find(btnRemove),
   btnRSS: () => wrapper.find(btnRSS),
-  inputRSS: () => wrapper.find(inputRSS)
+  inputRSS: () => wrapper.find(inputRSS),
 });
 
-describe("Tabs", () => {
-  it("add tab", () => {
+describe('Tabs', () => {
+  it('add tab', () => {
     const wrapper = mount(<App />);
     const s = buildSelectors(wrapper);
     const tabsCount = s.tabs().length;
-    s.btnAdd().simulate("click");
+    s.btnAdd().simulate('click');
     expect(s.tabBox()).toContainMatchingElements(tabsCount + 1, tab);
   });
 
-  it("remove tab", () => {
+  it('remove tab', () => {
     const wrapper = mount(<App />);
     const s = buildSelectors(wrapper);
     const tabsCount = s.tabs().length;
     s.btnRemove()
       .at(2)
-      .simulate("click");
+      .simulate('click');
     expect(s.tabBox()).toContainMatchingElements(tabsCount - 1, tab);
   });
 
-  it("change tab", () => {
+  it('change tab', () => {
     const wrapper = mount(<App />);
     const s = buildSelectors(wrapper);
-    const tab = s.tabs().at(2);
-    tab.simulate("click");
+    s.tabs().at(2).simulate('click');
     const tabSelected = s.tabs().at(0);
     const tabNotSelected = s.tabs().at(2);
-    expect(tabSelected).toHaveProp("aria-selected", "false");
-    expect(tabNotSelected).toHaveProp("aria-selected", "true");
+    expect(tabSelected).toHaveProp('aria-selected', 'false');
+    expect(tabNotSelected).toHaveProp('aria-selected', 'true');
   });
 
-  test("save active tabs to cookies", () => {
+  test('save active tabs to cookies', () => {
     const cookiesStub = () => {
       const cookie = {};
       return {
         set: (field, value) => {
           cookie[field] = value;
         },
-        get: field => cookie[field]
+        get: field => cookie[field],
       };
     };
     const cookie = cookiesStub();
@@ -71,32 +69,32 @@ describe("Tabs", () => {
     const s = buildSelectors(wrapper);
     s.tabs()
       .at(2)
-      .simulate("click");
+      .simulate('click');
 
     const wrapper2 = mount(<App cookie={cookie} />);
     const s2 = buildSelectors(wrapper2);
 
-    expect(s2.tabs().at(2)).toHaveProp("aria-selected", "true");
+    expect(s2.tabs().at(2)).toHaveProp('aria-selected', 'true');
   });
 });
 
-describe("RSS parse", () => {
-  test("save tab with rss", async () => {
+describe('RSS parse', () => {
+  test('save tab with rss', async () => {
     const wrapper = mount(<App />);
     const s = buildSelectors(wrapper);
 
-    const CORS_PROXY = "https://cors.io";
+    const CORS_PROXY = 'https://cors.io';
     nock(CORS_PROXY)
-      .defaultReplyHeaders({ "access-control-allow-origin": "*" })
-      .get("/?http://www.example.com")
-      .replyWithFile(200, "__fixtures__/rss.xml");
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/?http://www.example.com')
+      .replyWithFile(200, '__fixtures__/rss.xml');
     const tabsCount = s.tabs().length;
-    s.inputRSS().simulate("change", {
-      target: { value: "http://www.example.com" }
+    s.inputRSS().simulate('change', {
+      target: { value: 'http://www.example.com' },
     });
 
-    expect(s.inputRSS()).toHaveProp("value", "http://www.example.com");
-    s.btnRSS().simulate("click");
+    expect(s.inputRSS()).toHaveProp('value', 'http://www.example.com');
+    s.btnRSS().simulate('click');
 
     await delay(1000);
     wrapper.update();
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,24 +1,26 @@
-import React, { Component } from "react";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import uniqid from "uniqid";
-import Cookies from "js-cookie";
-import "babel-polyfill";
-import ParserRSS from "rss-parser";
-import "react-tabs/style/react-tabs.css";
+import React, { Component } from 'react';
+import {
+  Tab, Tabs, TabList, TabPanel,
+} from 'react-tabs';
+import uniqid from 'uniqid';
+import Cookies from 'js-cookie';
+import 'babel-polyfill';
+import ParserRSS from 'rss-parser';
+import 'react-tabs/style/react-tabs.css';
 
 class TabsCоmponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       tabs: [
-        { id: uniqid(), title: "Title 1", content: "Any content 1" },
-        { id: uniqid(), title: "Title 2", content: "Any content 2" },
-        { id: uniqid(), title: "Title 3", content: "Any content 3" },
-        { id: uniqid(), title: "Title 4", content: "Any content 4" },
-        { id: uniqid(), title: "Title 5", content: "Any content 5" }
+        { id: uniqid(), title: 'Title 1', content: 'Any content 1' },
+        { id: uniqid(), title: 'Title 2', content: 'Any content 2' },
+        { id: uniqid(), title: 'Title 3', content: 'Any content 3' },
+        { id: uniqid(), title: 'Title 4', content: 'Any content 4' },
+        { id: uniqid(), title: 'Title 5', content: 'Any content 5' },
       ],
       activeIndex: this.getSavedIndex(),
-      link: ""
+      link: '',
     };
   }
 
@@ -26,12 +28,12 @@ class TabsCоmponent extends Component {
     const { tabs } = this.state;
 
     return tabs.map(tab => (
-      <Tab key={tab.id} data-test='tab-item'>
+      <Tab key={tab.id} data-test="tab-item">
         {tab.title}
         <button
-          data-test='remove'
+          data-test="remove"
           onClick={() => this.handleClickRemoveTab(tab.id)}
-          type='button'
+          type="button"
         >
           Remove tab
         </button>
@@ -52,51 +54,46 @@ class TabsCоmponent extends Component {
   handleClickNewTab = () => {
     const { tabs } = this.state;
     this.setState({
-      tabs: [...tabs, { id: uniqid(), title: "Title ", content: "Any content" }]
+      tabs: [...tabs, { id: uniqid(), title: 'Title ', content: 'Any content' }],
     });
   };
 
-  handleChangeNewTabRSS = e => {
+  handleChangeNewTabRSS = (e) => {
     this.setState({ link: e.target.value });
   };
 
-  handleGetRSS = async e => {
+  handleGetRSS = async (e) => {
     e.preventDefault();
     const { link, tabs } = this.state;
-    let parser = new ParserRSS();
-    const CORS_PROXY = "https://cors.io/?";
-    try {
-      console.log(CORS_PROXY + link)
-      const feed = await parser.parseURL(CORS_PROXY + link);
+    const parser = new ParserRSS();
+    const CORS_PROXY = 'https://cors.io/?';
+    const feed = await parser.parseURL(CORS_PROXY + link);
 
-      const newContent = feed.items.map(item => item.title);
-      this.setState({
-        tabs: [...tabs, { id: uniqid(), title: link, content: newContent }],
-        activeIndex: tabs.length
-      });
-    } catch (error) {
-      console.log(error)
-      return error;
-    }
+    const newContent = feed.items.map(item => item.title);
+    this.setState({
+      tabs: [...tabs, { id: uniqid(), title: link, content: newContent }],
+      activeIndex: tabs.length,
+    });
   };
-  handleClickRemoveTab = id => {
+
+  handleClickRemoveTab = (id) => {
     this.setState(state => ({
       ...state,
-      tabs: state.tabs.filter(tab => tab.id !== id)
+      tabs: state.tabs.filter(tab => tab.id !== id),
     }));
   };
 
   getSavedIndex = () => {
-    const tabIndex = Cookies.get("tabIndex");
+    const tabIndex = Cookies.get('tabIndex');
     return parseInt(tabIndex, 10);
   };
 
-  setSavedIndex = index => {
+  setSavedIndex = (index) => {
     const { tabs } = this.state;
-    Cookies.set("tabIndex", Math.min(index, tabs.length - 1));
+    Cookies.set('tabIndex', Math.min(index, tabs.length - 1));
   };
 
-  handleTabSelect = activeIndex => {
+  handleTabSelect = (activeIndex) => {
     this.setState({ activeIndex });
     this.setSavedIndex(activeIndex);
   };
@@ -104,18 +101,18 @@ class TabsCоmponent extends Component {
   render() {
     const { activeIndex, link } = this.state;
     return (
-      <div data-test='box'>
-        <button data-test='add' onClick={this.handleClickNewTab}>
+      <div data-test="box">
+        <button data-test="add" type="submit" onClick={this.handleClickNewTab}>
           add
         </button>
         <form>
           <input
-            data-test='link-rss'
-            type='text'
+            data-test="link-rss"
+            type="text"
             onChange={this.handleChangeNewTabRSS}
             value={link}
           />
-          <button data-test='add-rss' onClick={this.handleGetRSS}>
+          <button data-test="add-rss" type="submit" onClick={this.handleGetRSS}>
             add link RSS
           </button>
         </form>
