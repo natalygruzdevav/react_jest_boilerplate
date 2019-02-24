2e2db2ca200eb3395bb1097cb925bb6932ab7980
Nataly Gruzdeva <gruzdeeva@restream.rt.ru>
2019-02-24 12:44:24 +0400

    test cookies

--- __tests__/tabs.test.jsx
+++ __tests__/tabs.test.jsx
@@ -1,42 +1,62 @@
-import React from "react";
-import { mount } from "enzyme";
+import React from 'react';
+import { mount } from 'enzyme';
+import 'react-log-state';
 
-import App from "../src/components/App";
+import App from '../src/components/App';
 
-describe("Tabs", () => {
-  it("add tab", () => {
+// eslint-disable-next-line
+ReactLogState.logAll();
+describe('Tabs', () => {
+  it('add tab', () => {
     const wrapper = mount(<App />);
     const tabsCount = wrapper.find('li[data-test="tab-item"]').length;
     const buttonAdd = wrapper.find('button[data-test="add"]');
-    buttonAdd.simulate("click");
+    buttonAdd.simulate('click');
     const tabsBox = wrapper.find('div[data-test="box"]');
     expect(tabsBox).toContainMatchingElements(
       tabsCount + 1,
-      'li[data-test="tab-item"]'
+      'li[data-test="tab-item"]',
     );
   });
 
-  it("remove tab", () => {
+  it('remove tab', () => {
     const wrapper = mount(<App />);
     const tabsCount = wrapper.find('li[data-test="tab-item"]').length;
-    const buttonRemove = wrapper.find('button[data-test="remove"]');
-    buttonRemove.simulate("click");
+    const buttonRemove = wrapper.find('button[data-test="remove"]').at(2);
+    buttonRemove.simulate('click');
     const tabsBox = wrapper.find('div[data-test="box"]');
     expect(tabsBox).toContainMatchingElements(
       tabsCount - 1,
-      'li[data-test="tab-item"]'
+      'li[data-test="tab-item"]',
     );
   });
 
-  it("change tab", () => {
+  it('change tab', () => {
     const wrapper = mount(<App />);
-    const tabsCount = wrapper.find('li[data-test="tab-item"]').length;
-    const buttonRemove = wrapper.find('button[data-test="remove"]');
-    buttonRemove.simulate("click");
-    const tabsBox = wrapper.find('div[data-test="box"]');
-    expect(tabsBox).toContainMatchingElements(
-      tabsCount - 1,
-      'li[data-test="tab-item"]'
-    );
+    const tab = wrapper.find('li[data-test="tab-item"]').at(2);
+    tab.simulate('click');
+    const tabSelected = wrapper.find('li[data-test="tab-item"]').at(0);
+    const tabNotSelected = wrapper.find('li[data-test="tab-item"]').at(2);
+    expect(tabSelected).toHaveProp('aria-selected', 'false');
+    expect(tabNotSelected).toHaveProp('aria-selected', 'true');;
+  });
+
+  test('save active tabs to cookies', () => {
+    const cookiesStub = () => {
+      const cookie = {};
+      return {
+        set: (field, value) => { cookie[field] = value; },
+        get: field => cookie[field],
+      };
+    };
+    const cookie = cookiesStub();
+
+    const wrapper = mount(<App cookie={cookie} />);
+    const tab = wrapper.find('li[data-test="tab-item"]').at(2);
+    tab.simulate('click');
+
+    const wrapper2 = mount(<App cookie={cookie} />);
+    const tab2 = wrapper2.find('li[data-test="tab-item"]').at(2);
+    expect(tab2).toHaveProp('aria-selected', 'true');
   });
 });
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,56 +1,97 @@
-import React, { Component } from "react";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import uniqid from "uniqid";
+import React, { Component } from 'react';
+import {
+  Tab, Tabs, TabList, TabPanel,
+} from 'react-tabs';
+import uniqid from 'uniqid';
+import Cookies from 'js-cookie';
 
-import "react-tabs/style/react-tabs.css";
+import 'react-tabs/style/react-tabs.css';
 
 class TabsCоmponent extends Component {
-  state = {
-    tabs: [
-      { id: uniqid(), title: "Title 1", content: "Any content 1" },
-      { id: uniqid(), title: "Title 2", content: "Any content 2" },
-      { id: uniqid(), title: "Title 3", content: "Any content 3" },
-      { id: uniqid(), title: "Title 4", content: "Any content 4" },
-      { id: uniqid(), title: "Title 5", content: "Any content 5" }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabs: [
+        { id: uniqid(), title: 'Title 1', content: 'Any content 1' },
+        { id: uniqid(), title: 'Title 2', content: 'Any content 2' },
+        { id: uniqid(), title: 'Title 3', content: 'Any content 3' },
+        { id: uniqid(), title: 'Title 4', content: 'Any content 4' },
+        { id: uniqid(), title: 'Title 5', content: 'Any content 5' },
+      ],
+      activeIndex: this.getSavedIndex(),
+    };
+  }
 
-  renderTabLink = () =>
-    this.state.tabs.map(tab => (
-      <Tab key={tab.id} data-test='tab-item'>
+  renderTabLink = () => {
+    const { tabs } = this.state;
+
+    return tabs.map(tab => (
+      <Tab key={tab.id} data-test="tab-item">
         {tab.title}
+        <button
+          data-test="remove"
+          onClick={() => this.handleClickRemoveTab(tab.id)}
+          type="button"
+        >
+          Remove tab
+        </button>
       </Tab>
     ));
+  };
 
-  renderTabContent = () =>
-    this.state.tabs.map(tab => (
+  renderTabContent = () => {
+    const { tabs } = this.state;
+
+    return tabs.map(tab => (
       <TabPanel key={tab.id}>
         <h2>{tab.content}</h2>
-        <button data-test="remove" onClick={() => this.handleClickRemoveTab(tab.id)} type='button'>Remove tab</button>
       </TabPanel>
     ));
+  };
 
   handleClickNewTab = () => {
+    const { tabs } = this.state;
     this.setState({
       tabs: [
-        ...this.state.tabs,
-        { id: uniqid(), title: "Title ", content: "Any content" }
-      ]
+        ...tabs,
+        { id: uniqid(), title: 'Title ', content: 'Any content' },
+      ],
     });
   };
+
   handleClickRemoveTab = (id) => {
     this.setState(state => ({
       ...state,
       tabs: state.tabs.filter(tab => tab.id !== id),
     }));
   };
+
+  getSavedIndex = () => {
+    const tabIndex = Cookies.get('tabIndex');
+    return parseInt(tabIndex, 10);
+  };
+
+  setSavedIndex = (index) => {
+    const { tabs } = this.state;
+    Cookies.set('tabIndex', Math.min(index, tabs.length - 1));
+  };
+
+  handleTabSelect = (activeIndex) => {
+    this.setState({ activeIndex });
+    this.setSavedIndex(activeIndex);
+  };
+
   render() {
+    const { activeIndex } = this.state;
     return (
       <div data-test="box">
-        <button data-test="add" onClick={this.handleClickNewTab} type='button'>
+        <button data-test="add" onClick={this.handleClickNewTab} type="button">
           Add tab
         </button>
-        <Tabs>
+        <Tabs
+          selectedIndex={activeIndex}
+          onSelect={this.handleTabSelect}
+        >
           <TabList>{this.renderTabLink()}</TabList>
           {this.renderTabContent()}
         </Tabs>
